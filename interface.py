aee84eee5e855df2f1b1c85ee4cfc2c8d73b2d19
Darth-coffee <138513619+Darth-coffee@users.noreply.github.com>
2025-04-24 19:45:41 +0530

    interface.py

--- interface.py
+++ interface.py
@@ -1,7 +1,7 @@
 from model import DeeperCNN as TheModel
-from train import train as the_trainer  # Your main training loop function is likely named `train`
-from predict import predict_single_image as the_predictor  # Replace with your actual function for prediction
+from train import train_model as the_trainer  # Your main training loop function is likely named `train`
+from predict import main as the_predictor  # Replace with your actual function for prediction
 from dataset import SyllableDataset as TheDataset
 from torch.utils.data import DataLoader as the_dataloader
-from config import batch_size as the_batch_size
-from config import epochs as total_epochs
+from config import BATCH_SIZE as the_batch_size
+from config import EPOCHS as total_epochs
