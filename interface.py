9fe5e5714efd4f6b7e88c462634fe99b1c5a1574
Darth-coffee <138513619+Darth-coffee@users.noreply.github.com>
2025-04-27 13:23:42 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,6 +1,6 @@
 from model import DeeperCNN as TheModel
 from train import train_model as the_trainer  # Your main training loop function is likely named `train`
-from predict import main as the_predictor  # Replace with your actual function for prediction
+from predict import return_preds_as_list as the_predictor  # Replace with your actual function for prediction
 from dataset import SyllableDataset as TheDataset
 from torch.utils.data import DataLoader as the_dataloader
 from config import BATCH_SIZE as the_batch_size
